fccdf380ea63b624e1137799b158e4143c6accc0
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 17:15:19 -0300

    Banner component added to the app

--- src/App.js
+++ src/App.js
@@ -1,10 +1,12 @@
 import Header from "components/Header";
 import CardList from "components/CardList";
+import Banner from "components/Banner";
 
 export default function App() {
   return (
     <div className="w-full h-screen">
       <Header />
+      <Banner />
       <CardList />
     </div>
   );
