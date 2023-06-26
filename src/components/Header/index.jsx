bcf7797d22d44577de603343508ae087dab69cd1
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 17:46:41 -0300

    bug fixed with the hamburguer menu and the logo

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -7,10 +7,10 @@ export default function Header() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="w-full bg-purple shadow-lg sticky top-0 flex flex-col md:flex-row items-start sm:items-center justify-between p-4 lg:px-48 md:py-4 md:px-16 z-40">
+    <header className="w-full bg-purple shadow-lg sticky top-0 flex flex-col md:flex-row items-start sm:items-center justify-between lg:px-48 md:py-4 md:px-16 z-40">
       <a
         href="/"
-        className="flex items-center text-3xl text-white cursor-pointer bg-purple"
+        className="flex items-center p-4 text-3xl text-white cursor-pointer bg-purple"
       >
         <MdTravelExplore className="drop-shadow" />
         <h1 className="text-inter drop-shadow">ReacTour</h1>
