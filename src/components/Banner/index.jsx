5750726921484f247fe5d68c6bdb39d42e070948
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 17:36:52 -0300

    component for banner created

---
+++ src/components/Banner/index.jsx
@@ -0,0 +1,33 @@
+import { AiOutlineSearch } from "react-icons/ai";
+
+export default function Banner() {
+  return (
+    <section className="p-4 sm:p-48 h-2/5 bg-[url('components/Banner/banner.jpg')] bg-cover flex flex-col sm:flex-row justify-between sm:items-center mb-4">
+      <div>
+        <p className="text-[#fff] text-[48px] tracking-tighter drop-shadow-sm">
+          Find{" "}
+          <strong>
+            <i>Adventures,</i>{" "}
+          </strong>
+          you want to talk about
+        </p>
+        <div className="flex ">
+          <input
+            type="text"
+            className="w-full p-4 rounded-l-lg"
+            placeholder="Search the best places"
+          />
+          <a href="" className=" bg-green rounded-r">
+            <AiOutlineSearch className="text-6xl p-4 cursor-pointer hover:scale-125 transition-all " />
+          </a>
+        </div>
+      </div>
+      <a
+        href="/"
+        className="text-[#fff] bg-black/[.2] px-4 py-2 hover:translate-y-2 transition-all"
+      >
+        How it works
+      </a>
+    </section>
+  );
+}
