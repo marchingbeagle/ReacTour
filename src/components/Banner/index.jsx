efb60098fe6ad1ad2186ee9eec489b67328252a9
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 17:44:35 -0300

    better style for banner on mobile

--- src/components/Banner/index.jsx
+++ src/components/Banner/index.jsx
@@ -2,9 +2,9 @@ import { AiOutlineSearch } from "react-icons/ai";
 
 export default function Banner() {
   return (
-    <section className="p-4 sm:p-48 h-2/5 bg-[url('components/Banner/banner.jpg')] bg-cover flex flex-col sm:flex-row justify-between sm:items-center mb-4">
+    <section className="p-4 sm:p-48 h-5/10 sm:2/5 bg-[url('components/Banner/banner.jpg')] bg-cover flex flex-col sm:flex-row justify-between sm:items-center mb-4">
       <div>
-        <p className="text-[#fff] text-[48px] tracking-tighter drop-shadow-sm">
+        <p className="text-[#fff] text-[40px] sm:text-[48px] tracking-tighter drop-shadow-xl py-5">
           Find{" "}
           <strong>
             <i>Adventures,</i>{" "}
