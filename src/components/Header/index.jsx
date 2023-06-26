07b73f6f74413e1217556f50f7ab92f96c2ede9a
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 17:15:35 -0300

    z index fixed

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -14,7 +14,7 @@ export default function Header() {
   const [open, setOpen] = useState(false);
 
   return (
-    <header className="w-full bg-purple shadow-lg sticky top-0 flex flex-col md:flex-row items-start sm:items-center justify-between p-4 lg:px-48 md:py-4 md:px-16 z-10">
+    <header className="w-full bg-purple shadow-lg sticky top-0 flex flex-col md:flex-row items-start sm:items-center justify-between p-4 lg:px-48 md:py-4 md:px-16 z-40">
       <a
         href="/"
         className="flex items-center text-3xl text-white cursor-pointer bg-purple"
