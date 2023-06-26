11dc73b8b9d93b59ace9f53071e64cf706c0a445
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 14:59:13 -0300

    import for the header and cardlist components

--- src/App.js
+++ src/App.js
@@ -1,3 +1,11 @@
+import Header from "components/Header";
+import CardList from "components/CardList";
+
 export default function App() {
-  return <div></div>;
+  return (
+    <div className="w-full h-screen">
+      <Header />
+      <CardList />
+    </div>
+  );
 }
