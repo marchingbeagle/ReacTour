b1ebff3e2e54464e25f9545f4efefb15d3afd280
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-26 08:56:17 -0300

    arquivos iniciais

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{js,jsx,ts,tsx}"],
+  theme: {
+    extend: {
+      colors: {
+        purple: "#B062F0",
+        green: "#A0D964",
+        yellow: "#E6D85E",
+        white: "#E7F1E8",
+        black: "#313131",
+      },
+    },
+  },
+  plugins: [],
+};
