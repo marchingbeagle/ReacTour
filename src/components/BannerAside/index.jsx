d09105cae078385258d8b381f53aaa3507d3dd25
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-27 12:37:09 -0300

    a few cleanup on the code

--- src/components/BannerAside/index.jsx
+++ src/components/BannerAside/index.jsx
@@ -1,5 +1,3 @@
-import bannerAside from "components/BannerAside/BannerAside.jpg";
-// <img className="mb-4 w-full" src={bannerAside} alt="Mount Everest" />
 function BannerAside() {
   return (
     <aside className="w-full bg-[url('components/BannerAside/BannerAside.jpg')] bg-cover bg-no-repeat md:w-1/3"></aside>
