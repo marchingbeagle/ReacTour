54100ec368c824742d597256a72afcc36b87929f
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-27 21:16:06 -0300

    logo component created to use on other components

--- src/components/Header/index.jsx
+++ src/components/Header/index.jsx
@@ -1,21 +1,14 @@
-import { MdTravelExplore } from "react-icons/md";
 import { useState } from "react";
 import NavList from "./NavList";
 import HamburguerMenu from "./HamburguerMenu";
+import Logo from "components/Logo";
 
 export default function Header() {
   const [open, setOpen] = useState(false);
 
   return (
     <header className="sticky top-0 z-40 flex w-full flex-col items-start justify-between bg-purple shadow-xl sm:items-center md:flex-row md:px-16 md:py-4 lg:px-48">
-      <a
-        href="/"
-        className="flex cursor-pointer items-center bg-purple p-4 text-3xl text-white"
-      >
-        <MdTravelExplore className="drop-shadow" />
-        <h1 className="text-inter drop-shadow">ReacTour</h1>
-      </a>
-
+      <Logo />
       <HamburguerMenu open={open} setOpen={setOpen} />
       <NavList open={open} />
     </header>
---
+++ src/components/Logo/index.jsx
@@ -0,0 +1,10 @@
+import { MdTravelExplore } from "react-icons/md";
+
+export default function Logo() {
+  return (
+    <div className="flex cursor-pointer items-center bg-purple p-4 text-3xl text-white">
+      <MdTravelExplore className="drop-shadow" />
+      <h1 className="text-inter drop-shadow">ReacTour</h1>
+    </div>
+  );
+}
