5ef394f51b6559511358a61bdf04fcf384be964e
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-06-28 19:07:38 -0300

    react router implemented

--- src/index.js
+++ src/index.js
@@ -1,11 +1,41 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
+import { RouterProvider, createBrowserRouter } from "react-router-dom";
+import Home from "routes/Home";
+import DefaultPage from "components/DefaultPage";
+import ErrorPage from "routes/ErrorPage";
 import "./index.css";
-import App from "./App";
+import AboutUs from "routes/AboutUs";
+import ContactUs from "routes/ContactUs";
+
+const router = createBrowserRouter([
+  {
+    path: "/",
+    element: <DefaultPage />,
+    children: [
+      {
+        path: "/",
+        element: <Home />,
+      },
+      {
+        path: "contactus",
+        element: <ContactUs />,
+      },
+      {
+        path: "aboutus",
+        element: <AboutUs />,
+      },
+      {
+        path: "*",
+        element: <ErrorPage />,
+      },
+    ],
+  },
+]);
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <App />
+    <RouterProvider router={router} />
   </React.StrictMode>
 );
