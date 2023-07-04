aed868272882ef54abf37a3c1cfd85eb94a6040a
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-07-04 13:59:32 -0300

    descriptions added to trips page and more styles

--- src/components/Logo/index.jsx
+++ src/components/Logo/index.jsx
@@ -1,10 +1,11 @@
 import { MdTravelExplore } from "react-icons/md";
+import { Link } from "react-router-dom";
 
 export default function Logo({ color = "purple" }) {
   return (
-    <div className={`flex items-center bg-${color} p-4 text-3xl text-white`}>
+    <Link className={`flex items-center bg-${color} p-4 text-3xl text-white`}>
       <MdTravelExplore className="drop-shadow" />
       <h1 className="text-inter drop-shadow">ReacTour</h1>
-    </div>
+    </Link>
   );
 }
--- src/routes/Trips/index.jsx
+++ src/routes/Trips/index.jsx
@@ -13,24 +13,30 @@ export default function Trips() {
   return (
     <div className="grow">
       <main>
-        <header>
-          <div className="h-12 bg-gray-200 flex items-center">
-            {currentTrip.continent}
+        <section className=" m-auto md:flex p-4 md:p-0 md:items-center md:w-2/3">
+          <div className="w-fit pb-8">
+            <div className="flex items-center gap-3 text-2xl py-4">
+              <h1>{currentTrip.name_of_place}</h1> -
+              <h2 className="text-lg">{currentTrip.country}</h2>
+            </div>
+            <img
+              className="rounded-t-xl"
+              src={currentTrip.path}
+              alt={`A photo of ${currentTrip.name_of_place}`}
+            />
+            <div className="flex py-2 justify-between sm:gap-4 shadow-md p-2 rounded-b-xl">
+              <FavoriteHeart trip={currentTrip} />
+              <div className="flex gap-2">
+                <span>{currentTrip.time_of_trip} trip</span>
+                <span className="border-l-2 border-purple pl-2">
+                  {currentTrip.number_of_people} people
+                </span>
+              </div>
+            </div>
           </div>
-          <div className="flex items-center gap-3 text-2xl pt-4">
-            <h1>{currentTrip.name_of_place}</h1> -
-            <h2 className="text-lg">{currentTrip.country}</h2>
-          </div>
-        </header>
-        <section>
-          <img
-            src={currentTrip.path}
-            alt={`A photo of ${currentTrip.name_of_place}`}
-          />
-          <span>{currentTrip.time_of_trip}</span>
-          <span>{currentTrip.number_of_people}</span>
-          <FavoriteHeart trip={currentTrip} />
+          <span className="md:w-2/3 md:p-8">{currentTrip.details}</span>
         </section>
+
         <section className="flex flex-col items-center justify-center w-5/6 sm:w-2/3 m-auto my-4">
           <h1 className="text-2xl text-gray-700 my-4 py-2 border-t-2 border-red uppercase flex items-center gap-2">
             Find the hottest trips
