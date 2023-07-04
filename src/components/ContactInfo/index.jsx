4bd1676f27ce4f1ecb83f07815d7470528b1936e
marchingbeagle <erik.schneider.pacheco@gmail.com>
2023-07-04 14:04:32 -0300

    about us translated to english

--- src/components/ContactInfo/index.jsx
+++ src/components/ContactInfo/index.jsx
@@ -10,7 +10,7 @@ function ContactInfo() {
         <li>CADASTUR: 998.425.740.552</li>
       </ul>
       <section className="my-4">
-        <h2 className="text-xl font-bold">Fale conosco:</h2>
+        <h2 className="text-xl font-bold">Contact us:</h2>
         <p className="my-4">(11) 3888-0902</p>
         <p className="my-4">terms@reactour.com</p>
       </section>
--- src/routes/AboutUs/index.jsx
+++ src/routes/AboutUs/index.jsx
@@ -4,24 +4,20 @@ function AboutUs() {
   return (
     <div className="grow w-5/6 sm:w-3/5 m-auto py-8">
       <h1 className="text-3xl tracking-wide">Details about us</h1>
-      <p className="my-4">
-        Informações da empresa, a partir de 4 de julho de 2023:
-      </p>
+      <p className="my-4">Company information, starting from July 4, 2023:</p>
       <p className="text-xl font-bold my-4">
-        Provedor do site e parte contratada para serviços de pagamento para
-        usuários residentes ou estabelecidos no Brasil:
+        Website provider and contracted party for payment services for users
+        residing or established in Brazil:
       </p>
       <ContactInfo />
-      <h2 className="text-xl font-bold">Seus dados pessoais</h2>
+      <h2 className="text-xl font-bold">Your personal data</h2>
       <p className="my-4">
-        Para que você possa exercer seus direitos em relação aos seus dados
-        pessoais, acesse aqui
+        To exercise your rights regarding your personal data, access here.
       </p>
       <p>
-        Caso você tenha alguma dúvida sobre privacidade ou proteção de dados no
-        ReacTour, pode entrar em contato com nosso Encarregado pelo Tratamento
-        de Dados (Data Protection Officer, DPO) através do email:
-        dpo@reactour.com
+        If you have any questions about privacy or data protection at ReacTour,
+        you can contact our Data Protection Officer (DPO) via email at:
+        dpo@reactour.com.
       </p>
     </div>
   );
